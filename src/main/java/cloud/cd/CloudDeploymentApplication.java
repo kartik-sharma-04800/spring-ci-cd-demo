0db15b7d2dcd72e8c6c4b7924f8bce33eec97348
Kartik <kartiks04800@gmail.com>
2026-04-22 14:49:17 +0530

    cloud_deployment

---
+++ src/main/java/cloud/cd/CloudDeploymentApplication.java
@@ -0,0 +1,13 @@
+package cloud.cd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudDeploymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudDeploymentApplication.class, args);
+    }
+
+}
---
+++ src/main/java/cloud/cd/controller/HelloController.java
@@ -0,0 +1,16 @@
+package cloud.cd.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello()
+    {
+        return "Hello From CI/CD Pipeline";
+    }
+
+}
